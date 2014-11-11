508e56f41c58649e066436d5f94194d6657c048e
Ryosuke Otsuya <r.otsuya@gmail.com>
2014-11-11 21:31:33 +0900

    gruntとnpmの設定を整理、不要モジュールを削除

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,12 +16,6 @@ module.exports = function (grunt) {
 
         // Metadata.
         pkg: grunt.file.readJSON('package.json'),
-        banner: '/*!\n' +
-            ' * Bootstrap v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-            ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
-            ' */\n',
-        // NOTE: This jqueryCheck code is duplicated in customizer.js; if making changes here, be sure to update the other copy too.
 
         // Task configuration.
         less: {
