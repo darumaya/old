552a716dbbea8b4988cb30b6f965790ac16ff86b
Ryosuke Otsuya <r.otsuya@gmail.com>
2014-12-26 02:07:44 +0900

    jumbotronの画質を向上

--- Gruntfile.js
+++ Gruntfile.js
@@ -162,9 +162,38 @@ module.exports = function (grunt) {
                     src: ['**/*.JPG', '!**/*-jumbotron-*.{jpg,JPG}'],
                     dest: 'images/'
                 }]
-            },            thumbnail: {
+            },
+            thumbnail: {
                 options: {
                     sizes: [{
+                        name: 'i6',
+                        width: 414,
+                        quality: 80
+                    }, {
+                        name: 'i6-2x',
+                        width: 828,
+                        quality: 30
+                    }, {
+                        name: 'xs',
+                        width: 768,
+                        quality: 80
+                    }, {
+                        name: 'xs-2x',
+                        width: 1536,
+                        quality: 30
+                    }, {
+                        name: 'sm',
+                        width: 992,
+                        quality: 80
+                    }, {
+                        name: 'hd',
+                        width: 1920,
+                        quality: 30
+                    }, {
+                        name: 'md',
+                        width: 1200,
+                        quality: 80
+                    }, {
                         name: '100',
                         width: 100,
                         height: 100,
@@ -176,8 +205,7 @@ module.exports = function (grunt) {
                         height: 200,
                         aspectRatio: false,
                         quality: 50
-                    }]
-                },
+                    }]                },
                 files: [{
                     expand: true,
                     cwd: 'src/images/',
